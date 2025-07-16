5f7a07c2122292210e2c5a8055de465c43e5bf68
ashankdavid <ashank2507david@gmail.com>
2025-07-16 16:05:48 +0530

    Just added all the programs from list, tuples, sets, dictionary

--- Collections/List/List.py
+++ Collections/List/List.py
@@ -1,42 +1,57 @@
-# List is Mutable, Ordered collection of data
+# Definition: A list is an ordered, mutable collection of items.
 
-# Properties
+# Properties:
 # Ordered
 # Mutable
-# Allows Duplicates
+# Allows duplicate elements
 
-# In other Languages we have arrays -----> int arr[]
+mylist = [1, 2, 3, 4, 5]  # Syntax of list using []
+anotherlist = list(range(2, 101, 2))
 
-a = [1, "Hello", 10.5, True]
+# Operations of List in Python
+# 1) Access Elements from mylist
+print("on 3rd index we have ", mylist[3])  # reference operator []
+print("on 0th index we have ",mylist[0])
+print(mylist[2:4])
+print("on Last index we have ",mylist[-1])
 
-myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-anotherList = list(range(2,101,2))
+# 2) Modifying the Element
+# Because Lists are mutable we can Modify the list
+mylist[3] = 400
 
-# 1) Accessing the Elements
-print(myList)
-print(anotherList)
-print(myList[2])
-print(myList[2:8]) # Taking Slice out of the main list
-print(myList[-8:-3])
-print(myList[:8])
-print(myList[2:])
+# 3) printing the list
+# 2 ways of printing lists or any collection in python
+# 1st way:
+print(mylist)
+# 2nd way:
+for i in mylist:
+    print(i, end=" ")
+print()
 
-# 2) Modifying the List
-myList[-2] = 300
-myList.append(11)
-print(myList)
-myList.insert(5, 3000)
-myList.insert(6, 5000)
-print(myList)
-myList.remove(5000)
-print(myList)
-myList.pop()
-print(myList)
+# 4) adding the elements to a list
+# append()  it will add an element at the last of the list
+mylist.append(6)
+mylist.append(7)
+for i in range(8, 11):
+    mylist.append(i)
+print(mylist)
 
-# 3) Other Functions
-print(len(myList))
-myList.sort()
-print(myList)
+# insert()  it will add the element at the particular index
+mylist.insert(6, 5000)
+mylist.insert(3, "CodersArcade")
+mylist.insert(11, "HII")
+print(mylist)
 
-myList.sort(reverse=True)
-print(myList)
+# 5) Removing an element from the list
+mylist.remove(5000)
+mylist.pop()
+del mylist[3]
+print(mylist)
+
+# 6) other functions
+print(len(mylist))  # this will give u the length of the list
+mylist.sort(reverse=True)  # this will sort the elements in the list
+print(mylist)
+
+
+print(anotherlist)
